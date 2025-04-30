bdeca4671f742ccdbac00570d45726bd8d507e32
Jyothsna Bhukya <jyothsnabhukya022@gmail.com>
2025-04-30 17:00:24 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,33 +1,36 @@
 
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import PostsList from "./features/posts/PostsList";
-import CreatePost from "./features/posts/CreatePost";
-import EditPost from "./features/posts/EditPost";
-import SinglePostPage from "./features/posts/SinglePostPage";
-import UsersPage from "./features/users/UsersPage";
-import UserPosts from "./features/users/UserPosts";
-import NotificationsList from "./features/notifications/NotificationsList";
+// App.js
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import PostsList from './features/posts/PostsList';
+import SinglePostPage from './features/posts/SinglePostPage';
+import EditPost from './features/posts/EditPost';
+import UsersPage from './features/users/UsersPage';
+import UserPosts from './features/users/UserPosts';
+import NotificationsList from './features/notifications/NotificationsList';
+import './styles/App.css';
 
 const App = () => {
   return (
-    <div className="App">
-      <h1>GenZ</h1>
-      <nav>
-        <a href="/">Posts</a> |{" "}
-        <a href="/users">Users</a> |{" "}
-        <a href="/notifications">Notifications</a>
-      </nav>
-      <Routes>
-        <Route path="/" element={<PostsList />} />
-        <Route path="/posts/:postId" element={<SinglePostPage />} />
-        <Route path="/editPost/:postId" element={<EditPost />} />
-        <Route path="/users" element={<UsersPage />} />
-        <Route path="/users/:userId" element={<UserPosts />} />
-        <Route path="/notifications" element={<NotificationsList />} />
-      </Routes>
-    </div>
+    <Router>
+      <div className="App">
+        <h1>GenZ</h1>
+        <nav>
+          <a href="/">Posts</a> | <a href="/users">Users</a> | <a href="/notifications">Notifications</a>
+        </nav>
+        <Routes>
+          <Route path="/" element={<PostsList />} />
+          <Route path="/posts/:postId" element={<SinglePostPage />} />
+          <Route path="/editPost/:postId" element={<EditPost />} />
+          <Route path="/users" element={<UsersPage />} />
+          <Route path="/users/:userId" element={<UserPosts />} />
+          <Route path="/notifications" element={<NotificationsList />} />
+        </Routes>
+      </div>
+    </Router>
   );
 };
 
 export default App;
+
+   
