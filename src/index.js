8e5565bb10c3583e4cf265f97da5cad2024997b7
Jyothsna Bhukya <jyothsnabhukya022@gmail.com>
2025-06-18 12:54:45 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
-// index.js
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './store';
-import App from './App';
-import './styles/index.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import store from './app/store'
+import { Provider } from 'react-redux'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
-);
+import { fetchUsers } from './features/users/usersSlice'
+
+import './api/server'
+
+store.dispatch(fetchUsers())
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
+)
